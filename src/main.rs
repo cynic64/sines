271852921a5ef9710ea49219fb023a6ff0a678c4
cynic64 <f3bl@gmx.com>
2020-03-08 23:58:17 +0100

    works on 2d stuff

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use rustfft::FFTplanner;
 
 use minifb::{Key, Window, WindowOptions};
 
+use rand::{Rng, thread_rng};
+
 const WIDTH: usize = 100;
 const HEIGHT: usize = 100;
 const SCALE: usize = 10;
@@ -11,34 +13,41 @@ const SCALE: usize = 10;
 const PI2: f64 = std::f64::consts::PI * 2.0;
 
 fn main() {
+    let mut rng = thread_rng();
+
     let mut input_2d = [[0.0; WIDTH]; HEIGHT];
     let u = 1.0;
-    let v = 2.0;
-    let freq = 10.0;
+    let v = 1.0;
+    let freq = 50.0;
     (0..HEIGHT).for_each(|wx| {
         (0..WIDTH).for_each(|wy| {
+            /*
             let x = wx as f64 / WIDTH as f64;
             let y = wy as f64 / WIDTH as f64;
 
             let n = ((u * (x * freq) + v * (y * freq)) * PI2).cos()
                 + ((u * (x * freq) + v * (y * freq)) * PI2).sin();
+
+            // let n = rng.gen();
             input_2d[wy][wx] = n;
+             */
+
+            let x = wx as f64 / WIDTH as f64;
+            let y = wy as f64 / WIDTH as f64;
+            let d = ((x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5)).sqrt();
+
+            if d < 0.1 {
+                input_2d[wy][wx] = 1.0;
+            } else {
+                input_2d[wy][wx] = 0.0;
+            }
         })
     });
 
-    /*
-    let mut output_2d = [[0.0; WIDTH]; HEIGHT];
-    (0..HEIGHT).for_each(|y| {
-        (0..WIDTH).for_each(|x| {
-            let r = output_1d[x].re;
-            let i = output_1d[x].im;
-            output_2d[y][x] = r * r + i * i;
-        })
-    });
-    */
+    let output_2d = fourier_2d(&input_2d);
 
     display_fb(&input_2d);
-    // display_fb(&output_2d);
+    display_fb(&output_2d);
 }
 
 fn normalize(transform: &mut [Complex<f64>]) {
@@ -50,20 +59,68 @@ fn normalize(transform: &mut [Complex<f64>]) {
     });
 }
 
-fn fourier(input: Vec<f64>) -> Vec<Complex<f64>> {
-    // calls fft.process and normalizes the output
-    let mut processed_input: Vec<_> = input.iter().map(|x| Complex { re: *x, im: 0.0 }).collect();
+fn fourier(input: &[Complex<f64>]) -> Vec<Complex<f64>> {
+    // odd-numbered inputs will cause problems
+    assert!(input.len() % 2 == 0);
 
+    let mut our_input = input.to_vec();
+
+    // calls fft.process and normalizes the output
     let length = input.len();
     let mut output: Vec<Complex<f64>> = vec![Complex::zero(); length];
 
     let mut planner = FFTplanner::new(false);
     let fft = planner.plan_fft(input.len());
-    fft.process(&mut processed_input, &mut output);
+    fft.process(&mut our_input, &mut output);
 
     normalize(&mut output);
 
-    output
+    let half_len = output.len() / 2;
+
+    (0..input.len())
+        .map(|idx| if idx < half_len {
+            output[half_len - idx - 1]
+        } else {
+            output[idx - half_len]
+        })
+        .collect()
+}
+
+fn fourier_2d(input: &[[f64; WIDTH]; HEIGHT]) -> [[f64; WIDTH]; HEIGHT] {
+    // convert everything to complex numbers
+    let complex_input: Vec<Vec<Complex<f64>>> = input
+        .iter()
+        .map(|row| row.iter().map(|&x| Complex { re: x, im: 0.0 }).collect())
+        .collect();
+
+    // so that we can compute the FFT of columns later (we do rows first), this
+    // is kinda backwards: the initial FFT results are stored vertically, not
+    // horizontally
+    let mut fourier_columns = [[Complex::zero(); WIDTH]; HEIGHT];
+
+    complex_input.iter().enumerate().for_each(|(y, row)| {
+        let row_output = fourier(row);
+
+        row_output
+            .iter()
+            .enumerate()
+            // note [x][y] instead of [y][x] - again to be able to compute
+            // FFT of columns easily later
+            .for_each(|(x, &point)| fourier_columns[x][y] = point);
+    });
+
+    let mut output_2d = [[0.0; WIDTH]; HEIGHT];
+
+    fourier_columns.iter().enumerate().for_each(|(y, column)| {
+        let column_output = fourier(column);
+
+        column_output
+            .iter()
+            .enumerate()
+            .for_each(|(x, &point)| output_2d[y][x] = point.re * point.re + point.im * point.im);
+    });
+
+    output_2d
 }
 
 fn hex(r: u32, g: u32, b: u32) -> u32 {
@@ -78,8 +135,12 @@ fn ghex(v: u32) -> u32 {
 }
 
 fn f64_to_hex(x: f64) -> u32 {
-    let x = 1.0 / (1.0 + std::f64::consts::E.powf(-x));
+    let contrast_factor = 10_000.0;
+
+    let x = 1.0 / (1.0 + std::f64::consts::E.powf(-(x * contrast_factor)));
+
     let mut v = ((x + 1.0) * 127.0) as i32;
+
     if v > 255 {
         v = 255
     } else if v < 0 {
